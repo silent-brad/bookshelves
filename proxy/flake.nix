0bc7008a0b3051744f34510b68b22cd884198aa0
bwhtdev <bradscottwhite@gmail.com>
2025-09-29 18:44:37 -0400

    removed unnecessary line in start proxy script

--- proxy/flake.nix
+++ proxy/flake.nix
@@ -23,7 +23,7 @@
             cp Caddyfile.template $out/data/Caddyfile.template
             cat > $out/bin/start-proxy <<EOF
             #!/usr/bin/env ${pkgs.bash}/bin/bash
-            \$(dirname "\$0")/app &
+            #\$(dirname "\$0")/app &
             echo "Starting reverse proxy"
             TEMPLATE_PATH="\$(dirname "\$0")/../data/Caddyfile.template"
             CONFIG_PATH="/tmp/Caddyfile"
